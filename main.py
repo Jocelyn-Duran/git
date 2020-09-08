2aa2a330525e783d6a69a9f89d52b2750f89d350
Jocelyn Minerva Duran Martinez <za16011248@zapopan.tecmm.edu.mx>
2020-09-08 18:30:40 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'mi nombre es, {name}') # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'mi num de control es 16011248')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Jocelyn Minerva Duran Martinez')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
